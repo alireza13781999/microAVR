e401f3690b0004e3bb15cb20766c70be552bb9a4
alireza13781999 <alirezanemati1999@gmail.com>
2021-01-01 13:05:27 +0330

    adding hello world in LCD project

---
+++ 3/Hello/code/lcd_hello.c
@@ -0,0 +1,44 @@
+/*******************************************************
+This program was created by the
+CodeWizardAVR V3.12 Advanced
+Automatic Program Generator
+© Copyright 1998-2014 Pavel Haiduc, HP InfoTech s.r.l.
+http://www.hpinfotech.com
+
+Project : LCD_Hello
+Version : 
+Date    : 01/01/2021
+Author  : 
+Company : 
+Comments: 
+
+
+Chip type               : ATmega16
+Program type            : Application
+AVR Core Clock frequency: 12/000000 MHz
+Memory model            : Small
+External RAM size       : 0
+Data Stack size         : 256
+*******************************************************/
+#include <mega16.h>
+
+// Alphanumeric LCD functions
+#include <alcd.h>
+
+
+
+void main(void)
+{
+
+DDRA = 0x00;
+PORTA = 0x00;
+
+lcd_init(15);
+
+while (1)
+      {
+            lcd_gotoxy(4,0);
+            lcd_putsf("Hello World");
+
+      }
+}
